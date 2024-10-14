e5292c931225a94b8ac5e5cf2e1a31db3891bd08
Geetanshi23 <geetanshipahuja@gmail.com>
2024-10-14 15:16:59 +0530

    first commit

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class PrimitiveDataTypes
+{
+	public static void main(String[] args) {
+	    int n = 5;
+	    double d = 32.2;
+	    float f = 1.32f;
+	    boolean ans = true;
+	    short x = 10000;
+	    long a = 100000L;
+	    byte c = 100;
+	    char ch = 'g';
+		System.out.println(n);
+		System.out.println(d);
+		System.out.println(f);
+		System.out.println(ans);
+		System.out.println(x);
+		System.out.println(a);
+		System.out.println(c);
+		System.out.println(ch);
+	}
+}
